6ce2ab64e317c246fce59fcf856816720dc62f0f
Nicholas Phandinata <nicztheone99@gmail.com>
2022-04-14 21:57:36 +0700

    Update cart feature

--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import redirect, render_template, url_for, flash, request, session, current_app
 from shop import app, mysql
 
@@ -22,7 +23,9 @@ def AddCart():
 
         if product_id and quantity and color and request.method == "POST":
               DictItems = {product_id:{'name':product[1],'price':product[2],
-              'discount':product[3],'color':color,'quantity':quantity,'image':product[10]}}
+              'discount':product[3],'color':color,'quantity':quantity,'image':product[10], 
+              'colors':product[5], 'quantities':product[4]}}
+              print(DictItems)
 
               if 'Shoppingcart' in session:
                     print(session['Shoppingcart'])
@@ -51,4 +54,38 @@ def getCart():
         subtotal += float(product['price']) * int(product['quantity'])
         subtotal -= discount
     grandtotal = "{:,.2f}".format(subtotal)
-    return render_template('products/carts.html', grandtotal=grandtotal)
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT DISTINCT(P.Brand_Id) Brand_Id, B.Name FROM Products P JOIN Brands B ON P.Brand_Id = B.Brand_Id")
+    displayBrands = cur.fetchall()
+
+    cur.execute("SELECT DISTINCT(P.Cat_Id) Cat_Id, C.Name FROM Products P JOIN Categories C ON P.Cat_Id = C.Cat_Id")
+    displayCategories = cur.fetchall()
+
+    return render_template('products/carts.html', grandtotal=grandtotal, displayBrands=displayBrands, displayCategories=displayCategories)
+
+@app.route('/updatecart/<int:code>', methods=['POST'])
+def UpdateCart(code):
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method =="POST":
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key , item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+@app.route('/clearcart')
+def clearcart():
+    try:
+        session.pop('Shoppingcart', None)
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
