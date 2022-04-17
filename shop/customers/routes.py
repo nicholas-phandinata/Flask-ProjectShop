4457b1edfee97abec02bd388a569cc979c5b2b83
Nicholas Phandinata <nicztheone99@gmail.com>
2022-04-17 22:19:23 +0700

    Customer checkout cart

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -6,7 +6,8 @@ import os
 
 @app.route('/customer/register', methods=['GET','POST'])
 def customer_register():
-    session.pop("username", None)
+    session.pop("customer", None)
+    session.pop("customer_id", None)
     form = CustomerRegisterForm(request.form)
     if request.method == 'POST' and form.validate():
           name = form.name.data
@@ -35,17 +36,19 @@ def customer_register():
 @app.route('/customer/login', methods=['GET','POST'])
 def customerLogin():
     session.pop("customer", None)
+    session.pop("customer_id", None)
     form = CustomerLoginFrom(request.form)
     if request.method == 'POST' and form.validate():
         email = form.email.data
         password = form.password.data
         cur = mysql.connection.cursor()
-        res = cur.execute("SELECT Pwd FROM Customers WHERE Email = %s", [email])
+        res = cur.execute("SELECT Customer_ID, Pwd FROM Customers WHERE Email = %s", [email])
         if res > 0:
             data = cur.fetchone()
-            hash_password = data[0]
+            hash_password = data[1]
             if bcrypt.check_password_hash(hash_password, password):
                 session["customer"] = email
+                session["customer_id"] = data[0]
                 flash(f"Hello {email}, Welcome!", "success")
                 return redirect(request.args.get('next') or url_for('home'))
             else:
@@ -59,5 +62,48 @@ def customerLogin():
 @app.route('/customer/logout', methods=['GET','POST'])
 def customerLogout():
     session.pop("customer", None)
+    session.pop("customer_id", None)
     flash(f"Logged out successfully!", "success")
     return redirect(request.args.get('next') or url_for('home'))
+
+@app.route('/getorder')
+def get_order():
+    if 'customer' in session:
+        cur = mysql.connection.cursor()
+        invoice = secrets.token_hex(5)
+        try:
+            sql_query = "INSERT INTO Customer_Orders (Invoice, Customer_ID, Orders) VALUES (%s, %s, %s)"
+            values = (invoice, session["customer_id"], session['Shoppingcart'])
+            cur.execute(sql_query, values)
+            mysql.connection.commit()
+            cur.close()
+            session.pop('Shoppingcart')
+            flash('Your order has been sent successfully','success')
+            return redirect(url_for('home'))
+        except Exception as e:
+            print(e)
+            flash('Some thing went wrong while get order', 'danger')
+            return redirect(url_for('getCart'))
+    elif 'customer' not in session:
+        return redirect(url_for('customerLogin'))
+
+@app.route('/orders/<invoice>')
+def orders(invoice):
+    if 'customer' in session:
+        grandTotal = 0
+        subTotal = 0
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT Customer_ID FROM Customers WHERE Email = %s", [session["customer"]])
+        get_customer_id = cur.fetchone()
+        # customer = Register.query.filter_by(id=customer_id).first()
+        # orders = CustomerOrder.query.filter_by(customer_id=customer_id, invoice=invoice).order_by(CustomerOrder.id.desc()).first()
+        for _key, product in orders.orders.items():
+            discount = (product['discount']/100) * float(product['price'])
+            subTotal += float(product['price']) * int(product['quantity'])
+            subTotal -= discount
+            tax = ("%.2f" % (.06 * float(subTotal)))
+            grandTotal = ("%.2f" % (1.06 * float(subTotal)))
+
+    else:
+        return redirect(url_for('customerLogin'))
+    return render_template('customer/order.html', invoice=invoice, tax=tax,subTotal=subTotal,grandTotal=grandTotal,orders=orders)
